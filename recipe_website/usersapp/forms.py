cc300444e6ebdb715a249b96b5bd1c0ae734cf81
igor_pont <pontigor11@gmail.com>
2023-12-05 20:14:01 +0300

    Added user profile data update

--- recipe_website/usersapp/forms.py
+++ recipe_website/usersapp/forms.py
@@ -1,13 +1,30 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
-"""
-Форма при регистрации
-"""
+
 class UserRegisterForm(UserCreationForm):
-    email = forms.EmailField()
+    """Форма регистрации пользователя на сайте"""
 
+    email = forms.EmailField()
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):
+    """Обновление данных пользователем на сайте"""
+
+    email = forms.EmailField()
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    """Обновление аватара пользователем на сайте"""
+
+    class Meta:
+        model = Profile
+        fields = ['image']
